1e7a166905b330f3fe8f48b59c8a719e786657c1
104062212 <karentseng1027@gmail.com>
2020-01-20 16:30:17 -0800

    first part of the lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,40 @@ $(document).ready(function() {
 	initializePage();
 })
 
+function projectClick(e) {
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+	//    description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+		description.fadeToggle();
+    }
+}
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
+		console.log("hey");
 		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("#myElement").click(changeText);
+
+	$("a.thumbnail").click(projectClick);
+}
+
+
+function changeText(event){
+	$(this).text("Changed text");
 }
